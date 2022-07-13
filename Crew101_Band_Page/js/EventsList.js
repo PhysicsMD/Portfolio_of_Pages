e4c22183897968c28bd80ddf08865edcb93b3dd4
Alenn Wright <bowserko123@gmail.com>
2022-07-12 18:13:21 -0700

    Create EventsList.js
    This is the events data for the past events section

---
+++ Crew101_Band_Page/js/EventsList.js
@@ -0,0 +1,40 @@
+const EventList = {
+    "past": [
+        {
+            'EventPlace':'Westgate Entertainment Stage',
+            'EventDate':'Friday, October 22, 2021',
+            'EventTime':'7:00pm to 9:00pm',
+            'EventGmaps':'https://g.page/Westgate-Entertainment-Glendale?share',
+            'EventGmapPhoto':'https://westgateaz.com/wp-content/uploads/2020/02/westgate-fb.jpg'
+        },
+        {
+            'EventPlace':'Desert Ridge Stage',
+            'EventDate':'Saturday, October 9, 2021',
+            'EventTime':'6:00pm to 9:00pm',
+            'EventGmaps':'https://goo.gl/maps/9wxaMftjH9Rj7uyEA',
+            'EventGmapPhoto':'https://shopdesertridge.com/wp-content/uploads/2022/05/503A8214.jpg'
+        },
+        {
+            'EventPlace':'State 48 Rock House',
+            'EventDate':'Friday September 17, 2021',
+            'EventTime':'9:00pm-12:00pm',
+            'EventGmaps':'https://goo.gl/maps/pVXhaMHdqoAypnxS6',
+            'EventGmapPhoto':'https://popmenucloud.com/cdn-cgi/image/width=300,height=300,format=auto,fit=scale-down/ecfdusht/063e0adf-4d04-474d-8d13-d6b5e2309d3d'
+        },
+        {
+            'EventPlace':"Dillon's KC BBQ Arrowhead",
+            'EventDate':'Saturday, October 2, 2021',
+            'EventTime':'6:00pm- 9:00pm',
+            'EventGmaps':'https://goo.gl/maps/rwSA7GSyYD6Tjgtj6',
+            'EventGmapPhoto':'https://dillonsrestaurant.com/wp-content/uploads/2017/04/logoweb1.jpg'
+        },
+        {
+            'EventPlace':"Cactus Jack's Bar & Grill",
+            'EventDate':'Saturday, January 1, 2022',
+            'EventTime':'7:30pm- 11:00pm',
+            'EventGmaps':'https://g.page/cactus-jack-s-bar-grill-phoenix?share',
+            'EventGmapPhoto':'https://media2.phoenixnewtimes.com/phx/imager/u/slideshow/11546724/cactus-jacks-ahwatukee-tavern-live-music-phoenix-ahwatukee.jpg'
+        }
+    ],
+    "upcoming": []
+} 
